d049ac2f17d28af8805ebcc33eb98712022db197
lukepark327 <lukepark327@gmail.com>
2022-11-05 21:48:32 +0900

    feat: script for trust up

---
+++ scripts/trustUp.sh
@@ -0,0 +1,12 @@
+for ((i=0;i<15;i++))
+do
+    echo "Running loop "$i
+    curl http://localhost:8545 -H\
+        "Content-type: application/json"\
+        --data '{"jsonrpc":"2.0","method":"evm_increaseTime","params":[361000], "id": 1}' # increase time
+    curl http://localhost:8545 -H\
+        "Content-type: application/json"\
+        --data '{"jsonrpc":"2.0","method":"hardhat_mine","params":["0x1"], "id": 1}' # mine one block
+    echo ""
+    dotenv -e .env.test -- npx hardhat run scripts/oracle.js --network localhost # set FOX price
+done
